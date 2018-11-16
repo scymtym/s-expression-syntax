a86c42db3d4481e998a0540b7d31b5e43e4c8fea
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-11-16 01:36:50 +0100

    Added slot stuff in standard-macros.lisp

--- src/definitions/standard-macros.lisp
+++ src/definitions/standard-macros.lisp
@@ -1,5 +1,9 @@
 (cl:in-package #:syntax)
 
+(parser:in-grammar special-operators)
+
+;;; `defun'
+
 (define-macro defun
     (list* (:guard name symbolp)
            (<- lambda-list ((ordinary-lambda-list lambda-lists) 'nil))
@@ -10,15 +14,54 @@
    (declarations  *>)
    (forms         *>)))
 
+;;; `defclass' including slots
+
+(parser:defrule slot-name ()
+  (:guard symbolp))
+
+(define-syntax slot-options
+    (list (* (or (:seq :reader        (<<- readers    (:must (function-name/symbol)))) ; TODO fail for something like :reader 1
+                 (:seq :writer        (<<- writers    (function-name)))
+                 (:seq :accessor      (<<- accessor   (function-name/symbol)))
+                 (:seq :allocation    (<-  allocation allocation-type))
+                 (:seq :initform      (<-  initform   :any)) ; TODO rule for form?
+                 (:seq :type          (<-  type       (type-specifier)))
+                 (:seq :documentation (:guard documentation string) ; TODO rule for documentation
+                       )
+                 (:seq (<<- option-names  (:guard symbolp))
+                       (<<- option-values :any)))))
+  ((readers       *)
+   (writers       *)
+   (accessor      *)
+   (allocation    ?)
+   (initform      ?)
+   (type          ?)
+   (documentation ?)
+   ;; Non-standard options
+   (option-names  *)
+   (option-values *)))
+
+(define-syntax slot-specifier
+    (or (<- name (slot-name))
+        (list* (<- name (slot-name)) (<- options (slot-options))))
+  ((name    1)
+   (options 1)))
+
 (define-macro defclass
     (list (:guard name symbolp)
           (list (* (<<- superclasses (:guard symbolp))))
-          (list (* (<<- slots (list (* :any)))))
-          (* (or (list :default-initargs (* (:seq (<<- default-initargs  (:guard symbolp))
-                                                  (<<- default-initforms :any))))
-                 (list :metaclass        (:guard metaclass symbolp))
-                 (list :documentation    (:guard documentation stringp))
-                 (list* (<<- option-names (:guard keywordp)) (<<- option-values)))))
+          (list (* (<<- slots (slot-specifier))))
+          (* (or ;; Standard options have their respective syntax.
+                 (list :default-initargs
+                       (* (:seq (<<- default-initargs  (:guard symbolp))
+                                (<<- default-initforms :any))))
+                 (list :metaclass
+                       (:guard metaclass symbolp))
+                 (list :documentation
+                       (:guard documentation stringp))
+                 ;; Non-standard options are basically free-form
+                 (list* (<<- option-names (:guard symbolp))
+                        (<<- option-values)))))
   ((name              1)
    (superclasses      *)
    (slots             *)
@@ -30,6 +73,8 @@
    ;; Non-standard options
    (option-names      *)
    (option-values     *)))
+
+;;; `defgeneric'
 
 (define-macro defgeneric
     (list name
