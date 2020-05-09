e25c33da3a407472651d55d3e7654aa4c1c6182a
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2020-05-09 15:14:38 +0200

    definitions: refactoring and better error messages in bindings

--- src/definitions/bindings.lisp
+++ src/definitions/bindings.lisp
@@ -1,6 +1,6 @@
 ;;;; bindings.lisp --- Rules for binding constructs.
 ;;;;
-;;;; Copyright (C) 2018, 2019 Jan Moringen
+;;;; Copyright (C) 2018, 2019, 2020 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
@@ -10,12 +10,15 @@
 
 (parser:defrule value-bindings ()
     (list (* (or (:transform
-                  (or (<<- names (variable-name))
+                  (or (<<- names (variable-name)) ; TODO repeated variable names
                       (list (<<- names (variable-name))))
                   (push nil values))
-                 (list (<<- names (variable-name))
-                       (<<- values)))))
+                 (list (:must (<<- names (variable-name)) "must be a variable name")
+                       (<<- values (form!))))))
   (list (nreverse names) (nreverse values)))
+
+(parser:defrule value-bindings! ()
+  (:must (value-bindings) "must be a list of bindings"))
 
 (parser:defrule symbol-macro-bindings ()
     (list (* (list (<<- names (variable-name))
@@ -25,10 +28,9 @@
 (parser:defrule local-function ()
     (list* (<- name (function-name))
            (<- lambda-list ((ordinary-lambda-list lambda-lists) 'nil)) ; TODO macro lambda-list for macrolet
-           (:compose (docstring-body) (list docstring declarations forms)))
+           (<- (docstring declarations forms) (docstring-body)))
   (list name (list 'parsed-lambda lambda-list docstring declarations forms)))
 
 (parser:defrule function-bindings ()
-    (list (* (:compose (local-function) (list (<<- names)
-                                              (<<- functions)))))
+    (list (* (<<- (names functions) (local-function))))
   (list (nreverse names) (nreverse functions)))
