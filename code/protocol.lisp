af92a9f353b4537953d87d9769eb8b566e29246c
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2021-08-06 15:10:25 +0200

    protocol: Add documentation strings

--- code/protocol.lisp
+++ code/protocol.lisp
@@ -6,17 +6,48 @@
 
 (cl:in-package #:s-expression-syntax)
 
-;;; Syntax meta-data protocol
+;;; Syntax description protocol
 
-(defgeneric find-syntax (name &key if-does-not-exist))
+(defgeneric find-syntax (name &key if-does-not-exist)
+  (:documentation
+   "Return the syntax description named NAME, if any.
+
+IF-DOES-NOT-EXIST controls the behavior in case a syntax description
+named NAME does not exist. The following values are allowed:
+
+#'ERROR
+
+  Signal an error if a syntax description named NAME does not exist.
+
+OBJECT
+
+  Return OBJECT if a syntax description named NAME does not exist."))
 
 (defgeneric (setf find-syntax) (new-value name &key if-does-not-exist))
+
+(defgeneric ensure-syntax (name class &rest initargs))
+
+(defgeneric name (thing)
+  (:documentation
+   "Return the name of THING.
+
+If THING is syntax description that describes a standard special
+operator, macro, class or type, the returned name is the symbol in the
+COMMON-LISP package which names the special operator, macro, class or
+type.
+
+If THING is a part, the name is a symbol which uniquely identifies the
+part within the containing syntax description."))
+
+(defgeneric parts (container)
+  (:documentation
+   "Return a sequence of parts belonging to CONTAINER.
+
+"))
 
 (defgeneric components (container))
 
 (defgeneric find-component (name container &key if-does-not-exist))
-
-(defgeneric ensure-syntax (name class &rest initargs))
 
 ;;; Default behavior
 
@@ -45,7 +76,40 @@
 
 ;;; Parse protocol
 
-(defgeneric parse (client syntax expression))
+(defgeneric classify (client expression)
+  (:documentation
+   "Classify EXPRESSION, possibly according to specialized behavior of CLIENT.
+
+Return a syntax description object that roughly reflects the kind of
+EXPRESSION. Note that a precise classification would have to take into
+account aspects beyond the syntax, such as the environment, to, for
+example, distinguish function and macro application or variable
+references and symbol macro applications. It should always be possible
+to find an appropriate syntax description:
+
++ If EXPRESSION is a special form, this function returns the syntax
+  description for the corresponding special operator.
+
++ If EXPRESSION is an application of a standard macro, this function
+  returns the syntax description for that macro.
+
++ If EXPRESSION a list not covered by the above cases, this function
+  returns the syntax description for a generic (that is, function or
+  macro) application. Note that this case also covers invalid
+  applications such as (1 2 3).
+
++ If EXPRESSION is a symbol but not a keyword, this function returns a
+  syntax description for a variable reference.
+
++ If EXPRESSION is any object that is not covered by the above cases,
+  this function returns a syntax description for a self-evaluating
+  object."))
+
+(defgeneric parse (client syntax expression)
+  (:documentation
+   "Parse EXPRESSION according to SYNTAX, possibly specialized behavior of CLIENT.
+
+TODO"))
 
 ;;; Default behavior
 
