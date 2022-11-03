e57c17d8ac5fb860e00ca38c9c252fa015fa07fd
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2022-11-03 01:12:42 +0100

    define-syntax: Allow specifying source variable

--- code/macros.lisp
+++ code/macros.lisp
@@ -1,16 +1,17 @@
 ;;;; macros.lisp --- Macros provided by the syntax system.
 ;;;;
-;;;; Copyright (C) 2018, 2019, 2020, 2021 Jan Moringen
+;;;; Copyright (C) 2018-2022 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package #:s-expression-syntax)
 
 (defmacro define-syntax (name-and-options syntax parts &rest options)
-  (destructuring-bind (name &key (grammar (parser.packrat.grammar:name
-                                           parser.packrat::*grammar*))
+  (destructuring-bind (name &key (grammar  (parser.packrat.grammar:name
+                                            parser.packrat::*grammar*))
                                  arguments
-                                 (kind    (a:make-keyword name)))
+                                 (kind     (a:make-keyword name))
+                                 (source   (gensym "SOURCE")))
       (a:ensure-list name-and-options)
     (let ((documentation (second (find :documentation options :key #'first)))
           (part-forms    '())
@@ -44,21 +45,20 @@
                                                             1 :initial-element ,evaluation)))))))
                            relations))))
            parts)
-      (a:with-unique-names (source)
-        `(progn
-           (parser.packrat:defrule (,name :grammar ,grammar)
-               (,@(map 'list #'first arguments))
-               (value (,source) ,syntax)
-             (bp:node* (,kind :source ,source)
-               ,@(nreverse relations)))
-           (ensure-syntax ',name 'special-operator
-                          :parts   (list ,@(nreverse part-forms))
-                          :rule    ,(if arguments
-                                        `'(,name ,@arguments)
-                                        `',name)
-                          :grammar ',grammar
-                          ,@(when documentation
-                              `(:documentation ,documentation))))))))
+      `(progn
+         (parser.packrat:defrule (,name :grammar ,grammar)
+             (,@(map 'list #'first arguments))
+           (value (,source) ,syntax)
+           (bp:node* (,kind :source ,source)
+                     ,@(nreverse relations)))
+         (ensure-syntax ',name 'special-operator
+                        :parts   (list ,@(nreverse part-forms))
+                        :rule    ,(if arguments
+                                      `'(,name ,@arguments)
+                                      `',name)
+                        :grammar ',grammar
+                        ,@(when documentation
+                            `(:documentation ,documentation)))))))
 
 (defmacro define-special-operator (name-and-options syntax &rest parts)
   (check-type syntax (cons (member list list*)))
